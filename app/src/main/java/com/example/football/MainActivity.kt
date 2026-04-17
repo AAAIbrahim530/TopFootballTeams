bfa7ac418b40f4f3b76579a0713236beaddfe19d
AAAIbrahim530 <07attiaibrahim@gmail.com>
2026-04-17 18:02:06 +0200

    added a function and a while loop

--- app/src/main/java/com/example/football/MainActivity.kt
+++ app/src/main/java/com/example/football/MainActivity.kt
@@ -15,15 +15,36 @@ class MainActivity : AppCompatActivity() {
         "Kaizer Chiefs",
         "Cape Town City FC")
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         // get the teams view
         val teamstextView = findViewById<TextView>(R.id.teamsTextView)
         // set the text view's text to the first team
         teamstextView.text = teams[0]
+        teams[0] = "Mamelodi Sundowns FC <3"
+        //add all the teams manually
+        var teamsDisplay = ""
+
+        var counter = 0
+        while (counter < teams.count())
+        {
+            teamsDisplay += "${teams[counter]}\n"
+            counter++
+            for (counter in 0..teams.count() - 1) {
+                teamsDisplay += "${teams[counter]}\n"
+            }
+            teamsDisplay += "${teams[0]}\n"
+            teamsDisplay += "${teams[1]}\n"
+            teamsDisplay += "${teams[2]}\n"
+            teamsDisplay += "${teams[3]}\n"
+            teamsDisplay += "${teams[4]}\n"
+
+            teamstextView.text = teamsDisplay
+        }
+
+
 
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
